bea234cc697059f6da60940a41d321007120da1b
doizz <doizz@naver.com>
2022-08-28 19:59:17 +0900

    페치 조인 1 - 기본

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,28 +14,40 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamA.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
 
             Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select t.members From Team t`";
+            String query = "select m From Member m join fetch m.team";
 
-            Collection result = em.createQuery(query, Collection.class).getResultList();
+            List<Member> result = em.createQuery(query, Member.class).getResultList();
 
-            System.out.println("result = " + result);
+            for (Member member : result){
+                System.out.println("member = " + member.getUsername() + " , " + member.getTeam().getName());
+            }
 
             tx.commit();
         } catch(Exception e){
@@ -220,7 +232,44 @@ public class JpaMain {
  *   - 가급적 묵시적 조인 대신에 명시적 조인 사용
  *   - 조인은 SQL 튜닝에 중요 포인트
  *   - 묵시적 조인은 조인이 일어나는 상황을 한눈에 파악하기 어려움
- *   
+ *
+ *  - JPQL - 페치조인
+ *   - SQL 조인 종류 X
+ *   - JPQL에서 성능 최적화를 위해 제공하는 기능
+ *   - 연관된 엔티티나 컬렉션을 SQl 한 번에 함께 조회하는 기능
+ *   - join fetch명령어 사용
+ *
+ *  - 엔티티 페치 조인
+ *   - 회원을 조회하면서 연관된 팀도 함께 조회 ( SQL 한 번에)
+ *   - SQL을 보면 회원 뿐만 아니라 팀(T.*)도 함께 select
+ *
+ *   - [JPQL]
+ *    - select m from Member m join fetch m.team
+ *
+ *   - [SQL]
+ *    - select M.*, T.* from member M inner join team T on m.TEAM_ID = T.ID
+ *
+ *  - 컬렉션 페치 조인
+ *   - 일대다 관계, 컬렉션 페치 조인
+ *   - [JPQL]
+ *    - select t from Team t join fetch t.members where t.name ='팀'
+ *   -[SQL]
+ *    - SELECT T.*, M.* FROM TEAM T INNER JOIN MEMBER M ON T.ID=M.TEAM_ID WHERE T.NAME = '팀A'
+ *
+ *  - 페치 조인과 DISTINCT
+ *   - SQL의 DISTINCT는 중복 결과를 제거하는 명령
+ *   - JPQL의 DISTINCT 2가지 기능 제공
+ *    - 1. SQL에 DISTINCT를 추가
+ *    - 2. 애플리케이션에서 엔티티 중복 제거
+ *
+ *  - 페치 조인과 일반 조인의 차이
+ *   - 일반 조인 실행시 연관된 엔티티를 함께 조회하지 않음
+ *   - JPQL은 결과를 반환할 때 연관관계 고려 X
+ *   - 단지 select 절에 지정한 엔티티만 조회할 뿐
+ *   - 여기서는 팀 엔티티만 조회하고, 회원 엔티티는 조회X
+ *
+ *  - 페치 조인을 사용할 때만 연관된 엔티티도 함께 조회(즉시로딩)
+ *  - 페치 조인은 객체 그래프를 SQL한번에 조회하는 개념
  *
  *
  */
