f7a9d49922ff168cfa2edbdb079bf6ec8f336da2
doizz <doizz@naver.com>
2022-09-05 20:52:56 +0900

    Named 쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -41,13 +41,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+
+            System.out.println("resultCount = " + resultCount);
+            System.out.println("member1.getAge() = " + member1.getAge());
+            System.out.println("member2.getAge() = " + member2.getAge());
+            System.out.println("member3.getAge() = " + member3.getAge());
 
             tx.commit();
         } catch(Exception e){
@@ -322,6 +323,26 @@ public class JpaMain {
  *  - Named쿼리 - 환경에 따른 설정
  *   - XML이 항상 우선권을 가진다.
  *   - 애플리케이션 운영 환경에 따른 XMl을 배포할 수 있다.
+ *
+ *  - 벌크 연산
+ *   - 재고가 10개 미만인 모든 상품의 가격을 10% 상승하려면?
+ *   - JPA 변경 감지 기능으로 실행하려면 너무 많은 SQL 실행
+ *    - 1. 재고가 10개 미만인 상품을 리스트로 조회한다.
+ *    - 2. 상품 엔티티의 가격을 10% 증가한다.
+ *    - 3. 트랜잭션 커밋 시점에 변경감지가 동작한다.
+ *   - 변경된 데이터가 100건 이라면 100번의 UPDATE SQL 실행
+ *
+ *   - 벌크 연산 예제
+ *    - 쿼리 한 번으로 여러 테이블 로우 변경(엔티티)
+ *    - EXECuteUpdate()의 결과는 영향받은 엔티티 수 반환
+ *    - UPDATE, DELETE 지원
+ *    - INSERT(insert into .. select, 하이버네이트 지원)
+ *
+ *   - 벌크 연산 주의
+ *    - 벌크 연산은 영속성 컨텍스트를 무시하고 데이터베이스에 직접 쿼리
+ *     - 벌크 연산을 먼저 실행
+ *     - 벌크 연산 수행 후 영속성 컨텍스트 초기화
+ *
  *
  *
  */
